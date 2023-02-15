d995e53c3194a16387c512efba2296b9076da1e1
ILikeProj <125489408+ILikeProj@users.noreply.github.com>
2023-02-15 19:51:19 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using System.IO.Enumeration;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello World!");
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //Debug.Log("Hello World!"); 
+       print("Hello World");
+
+    }
+   
+}
