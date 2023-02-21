7a84739bee02eb75f3e33c1e78a502fd54f3154a
ILikeProj <125489408+ILikeProj@users.noreply.github.com>
2023-02-21 20:16:08 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public int health = 3;
+
+    
+
+    public int level = 1;
+
+    public float speed = 1.2f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Задаёт кол-во хп
+        health = 3;
+        //Выводит хп NPC
+        print(health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+
+
+    }
+}
